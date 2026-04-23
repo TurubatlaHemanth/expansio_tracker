efffe3a0a79977268ebb54cbfd2e19acdbb5e4d7
TurubatlaHemanth <turubatlahemanth2002@gmail.com>
2026-04-23 12:57:30 +0530

    add cors for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const errorHandler = require("./middleWare/errorHandler");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://expansio-tracker.onrender.com","http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(jsonErrorHandler);
 
